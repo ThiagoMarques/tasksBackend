8e6d458234831ebd376264c91da0ea2810fe2403
Thiago Marques <thiagossmarques@gmail.com>
2023-09-07 19:00:32 -0300

    chore: insere migration para criação de tabela tasks

--- migrations/20230907214707_create_table_tasks.js
+++ migrations/20230907214707_create_table_tasks.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('tasks', table => {
+        table.increments('id').primary()
+        table.string('desc').notNullable()
+        table.string('estimateAt').notNullable()
+        table.string('doneAt').notNullable()
+        table.string('userId').references('id').inTable('users').notNullable()
+      })
 };
 
 /**
@@ -11,5 +17,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  knex.schema.dropTable('tasks')
 };
